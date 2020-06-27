5cf0b748fe15108f4f1b33e29d0922a7972eb983
aswin <aswinvijayan97@gmail.com>
2020-06-27 14:53:54 +0530

    added initial loading stage

--- src/PageTemplate/StoryPage.js
+++ src/PageTemplate/StoryPage.js
@@ -1,86 +1,110 @@
-import React, { useState } from 'react';
-import './StoryPage.css';
-import { db ,saveUserProgress} from '../config/Firebase'
+import React from "react";
+import "./StoryPage.css";
+import { db } from "../config/Firebase";
+
 class StoryPage extends React.Component {
     constructor(props) {
-        super(props)
-        console.log(props)
+        super(props);
         this.state = {
-            question: props.questions[0],
-            stage: 0,
+            question: {},
+            stage: -1,
+            loading: true,
             user: props.user
-        }
+        };
     }
-    saveUserProgress(uid,stage){
-         db.collection('users')
-        .doc(uid)
-        .get().then(function (user) {
-            if (user.exists) {
-                if(user.data().stages == null) {
-                    return
+    saveUserProgress(uid, stage) {
+        db.collection("users")
+            .doc(uid)
+            .get()
+            .then(function (user) {
+                if (user.exists) {
+                    if (user.data().stages == null) {
+                        return;
+                    }
+                    if (user.data().stages.indexOf(stage) === -1) {
+                        db.collection("users")
+                            .doc(uid)
+                            .set({ uid: uid, stages: user.data().stages.concat(stage) });
+                    }
                 }
-                if(user.data().stages.indexOf(stage) === -1) {
-                    db.collection('users')
-                    .doc(uid)
-                    .set({ uid: uid, stages : user.data().stages.concat(stage) })
-                }               
-            }
-        }) 
+            });
     }
     componentDidMount() {
-        var uid = this.state.user.uid
-        const upadatePage = (state) => {
-            this.setState(state)
-        }
-        const props  = this.props
-        db.collection('users')
+        var uid = this.state.user.uid;
+        const upadatePage = state => {
+            this.setState(state);
+        };
+        const props = this.props;
+        db.collection("users")
             .doc(uid)
-            .get().then(function (user) {
+            .get()
+            .then(function (user) {
                 if (user.exists) {
-                    var stage = user.data().stages.slice(-1)[0]
-                    upadatePage({ stage, question: props.questions[stage] });
+                    var stage = user.data().stages.slice(-1)[0];
+                    upadatePage({
+                        stage,
+                        question: props.questions[stage],
+                        loading: false
+                    });
                 } else {
-                    user = { uid, stages: [0]}
-                    db.collection('users')
+                    user = { uid, stages: [0] };
+                    db.collection("users")
                         .doc(user.uid)
-                        .set(user)
-                    upadatePage({ stage: 0, question: props.questions[0] });
+                        .set(user);
+                    upadatePage({
+                        stage: 0,
+                        question: props.questions[0],
+                        loading: false
+                    });
                 }
-            }).catch(function (error) {
+            })
+            .catch(function (error) {
                 console.log("Error getting document:", error);
             });
     }
     render() {
-        this.saveUserProgress(this.state.user.uid,this.state.stage)
-        return (
-            <div className="StoryPage" >
-                <div className="Question">
-                    <p>{this.state.question.text}</p>
-                </div>
-                {
-                    this.state.question.img ?
+        if (!this.state.loading) {
+            this.saveUserProgress(this.state.user.uid, this.state.stage);
+            return (
+                <div key={this.state.stage} className="StoryPage">
+                    <div className="Question">
+                        <p>{this.state.question.text}</p>
+                    </div>
+                    {this.state.question.img ? (
                         <img className="image" src={this.state.question.img} />
-                        : <></>
-                }
-                <div className="options">
-                    {
-                        this.state.question.options.map((item) => {
+                    ) : (
+                            <></>
+                        )}
+                    <div className="options">
+                        {this.state.question.options.map(item => {
                             return (
                                 <div>
-                                    <button className="button"
-                                        onClick={() => { console.log("clicked"); this.setState({ question: this.props.questions[item.actionIndex],stage:item.actionIndex }) }}
+                                    <button
+                                        className="button"
+                                        onClick={() => {
+                                            console.log("clicked");
+                                            this.setState({
+                                                question: this.props.questions[item.actionIndex],
+                                                stage: item.actionIndex
+                                            });
+                                        }}
                                     >
                                         {item.text}
                                     </button>
                                 </div>
-                            )
-                        })
-                    }
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div>
+                    <p>loading</p>
+                </div>
+            );
+        }
     }
-
 }
 
 export default StoryPage;
