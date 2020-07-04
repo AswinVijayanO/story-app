ceaac40626eecd41818b4b2ebc34a73ec2896342
vishnu <vishnuprasadkv55@gmail.com>
2020-07-04 11:12:55 +0530

    card styling for landing page

--- src/PageTemplate/LandingPage.js
+++ src/PageTemplate/LandingPage.js
@@ -3,6 +3,7 @@ import {
     Link
 } from "react-router-dom";
 import conf from '../questions.json'
+import StoryCard from "../config/components/StoryCard/StoryCard";
 function TopBar(props) {
     return (<div className="top-bar">
         <div className="button signout" onClick={props.signOut}></div>
@@ -13,18 +14,15 @@ export default function LandingPage(props) {
     var contests = conf.contests
     return (
         <div>
-            <ul>
                 {
                     contests.map((item, index) => {
                         console.log(item.gameName)
                         return (
-                            <li key={index}>
-                                <Link to={"/games/" + item.gameName}>{item.gameName}</Link>
-                            </li>
+                                <Link to={"/games/" + item.gameName}>
+                                <StoryCard gameDetails={item}></StoryCard></Link>
                         )
                     })
                 }
-            </ul>
 
         </div>
     );
