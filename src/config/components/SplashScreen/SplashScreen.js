2fb0763b0fc75ee70b614b31f728f6d394a9cc04
vishnu <vishnuprasadkv55@gmail.com>
2020-07-05 18:44:12 +0530

    modifying loading screen

--- src/config/components/SplashScreen/SplashScreen.js
+++ src/config/components/SplashScreen/SplashScreen.js
@@ -8,15 +8,15 @@ class SplashScreen extends React.Component {
 
         <div className="content">
           <div className="content__container">
-            <p className="content__container__text">
+            {/* <p className="content__container__text">
               Hello
-    </p>
+    </p> */}
 
             <ul className="content__container__list">
-              <li className="content__container__list__item">world !</li>
-              <li className="content__container__list__item">bob !</li>
-              <li className="content__container__list__item">users !</li>
-              <li className="content__container__list__item">everybody !</li>
+              <li className="content__container__list__item">Peace</li>
+              <li className="content__container__list__item">courage</li>
+              <li className="content__container__list__item">Faith</li>
+              <li className="content__container__list__item">specter</li>
             </ul>
           </div>
         </div>
