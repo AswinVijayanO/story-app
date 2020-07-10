6e01c04ee2655a3dfc615b9e21d3ab1aecc665ff
vishnu <vishnuprasadkv55@gmail.com>
2020-07-10 23:50:03 +0530

    styling landing page

--- src/PageTemplate/LandingPage.js
+++ src/PageTemplate/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./LandingPage.css";
 import {
     Link
 } from "react-router-dom";
@@ -7,15 +8,29 @@ import StoryCard from "../config/components/StoryCard/StoryCard";
 export default function LandingPage(props) {
     var contests = conf.contests
     return (
-        <div>
+        <div className="main-content">
+            <div className="head-section">
+                Hello There .. 
+            </div>
             {
-                contests.map((item, index) => {
+                <div className="game-section">
+                {contests.map((item, index) => {
                     console.log(item.gameName)
                     return (
                         <Link to={"/games/" + item.gameName}>
-                            <StoryCard gameDetails={item}></StoryCard></Link>
+                            <div className="for-shadow">
+                            <div className="card-bottom">
+                            <StoryCard  gameDetails={item}></StoryCard>
+                            </div>
+                            <div className="card-top">
+                            <StoryCard  gameDetails={item}></StoryCard>
+                            </div>
+                            </div>
+                                </Link>
                     )
                 })
+            }
+            </div>
             }
 
         </div>
