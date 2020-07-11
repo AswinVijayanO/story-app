e6cee13d4ad9cf23711faa23d03bae10ff28b922
aswin <aswinvijayan97@gmail.com>
2020-07-11 18:17:56 +0530

    modification for carousel games

--- src/PageTemplate/LandingPage.js
+++ src/PageTemplate/LandingPage.js
@@ -8,31 +8,34 @@ import StoryCard from "../config/components/StoryCard/StoryCard";
 export default function LandingPage(props) {
     var contests = conf.contests
     return (
-        <div className="main-content">
+        <div className="main-content" >
             <div className="head-section">
-                Hello There .. 
+                Hello There ..
             </div>
+            
             {
-                <div className="game-section">
-                {contests.map((item, index) => {
-                    console.log(item.gameName)
-                    return (
-                        <Link to={"/games/" + item.gameName}>
-                            <div className="for-shadow">
-                            <div className="card-bottom">
-                            <StoryCard  gameDetails={item}></StoryCard>
-                            </div>
-                            <div className="card-top">
-                            <StoryCard  gameDetails={item}></StoryCard>
-                            </div>
-                            </div>
-                                </Link>
-                    )
-                })
-            }
-            </div>
-            }
-
+                <div className="game-section" >
+                   <p>Choose your adventure</p>
+                   <div style={{paddingLeft:20,flexDirection: 'row', display: 'flex', width: '100vw', overflow: 'scroll', flex: 1 }}>
+                    {contests.map((item, index) => {
+                        console.log(item.gameName)
+                        return (
+                            <Link className='Link' to={"/games/" + item.gameName}>
+                                <div className="for-shadow">
+                                    <div className="card-bottom">
+                                        <StoryCard gameDetails={item}></StoryCard>
+                                    </div>
+                                    <div className="card-top">
+                                        <StoryCard gameDetails={item}></StoryCard>
+                                    </div>
+                                </div>
+                            </Link>
+                        )
+                    })
+                    }</div>
+                    <p>Create your adventure</p>
+                </div>
+            } 
         </div>
     );
 }
